c61a16a18820aed842d12d3614694d833503b891
JRKKRUPICKI <61666602+JRKKRUPICKI@users.noreply.github.com>
2020-04-16 12:26:27 +0200

    Add files via upload

--- cw5/cw5_z1.py
+++ cw5/cw5_z1.py
@@ -6,17 +6,15 @@ class Material:
         self.szerokosc = szerokosc
 
     def wyswietl_nazwe(self):
-        print(self.rodzaj)
+        print(f"{self.rodzaj} | {self.dlugosc} | {self.szerokosc}")
 
 class Ubrania(Material):
     
     def __init__(self, rozmiar, kolor, dla_kogo, rodzaj, dlugosc, szerokosc):
+        Material.__init__(self, rodzaj, dlugosc, szerokosc)
         self.rozmiar = rozmiar
         self.kolor = kolor
         self.dla_kogo = dla_kogo
-        self.rodzaj = rodzaj
-        self.dlugosc = dlugosc
-        self.szerokosc = szerokosc
 
     def wyswietl_dane(self):
         print(f"{self.rozmiar} | {self.kolor} | {self.dla_kogo} | {self.rodzaj} | {self.dlugosc} | {self.szerokosc}")
@@ -24,13 +22,8 @@ class Ubrania(Material):
 class Sweter(Ubrania):
     
     def __init__(self, rodzaj_swetra, rozmiar, kolor, dla_kogo, rodzaj, dlugosc, szerokosc):
+        Ubrania.__init__(self, rozmiar, kolor, dla_kogo, rodzaj, dlugosc, szerokosc)
         self.rodzaj_swetra = rodzaj_swetra
-        self.rozmiar = rozmiar
-        self.kolor = kolor
-        self.dla_kogo = dla_kogo
-        self.rodzaj = rodzaj
-        self.dlugosc = dlugosc
-        self.szerokosc = szerokosc
 
     def wyswietl_dane(self):
         print(f"{self.rodzaj_swetra} | {self.rozmiar} | {self.kolor} | {self.dla_kogo} | {self.rodzaj} | {self.dlugosc} | {self.szerokosc}")
